f7b945cd0f45f3ef1b210b140cac955bb9f6d993
Viviane <dessaintviviane@gmail.com>
2025-08-22 17:19:19 +0200

    Refactor discography page to improve album sorting logic and enhance code readability

--- pages/discography.page.js
+++ pages/discography.page.js
@@ -11,39 +11,72 @@ import { WEBSITE_NAME } from '../lib/constants';
 export default function Discography() {
 	// Order all the albums by release date (newest first)
 	// Release date is in each album object as releaseDate with format 2020-12-30T00:00:00+0200 (ISO 8601)
-	let albums = {};
-	Object.keys(albumsJson)
+	const legacyAlbums = {};
+	const albums = {};
+
+  Object.keys(albumsJson).forEach((key) => {
+    const album = albumsJson[key];
+    if (album.id.startsWith('KSDL')) {
+      legacyAlbums[key] = album;
+    } else {
+      albums[key] = album;
+    }
+  });
+
+  // Sort both albums and legacy albums by release date
+  const sortedAlbums = Object.keys(albums)
 		.sort((a, b) => {
-			const dateA = new Date(albumsJson[a].releaseDate);
-			const dateB = new Date(albumsJson[b].releaseDate);
+			const dateA = new Date(albums[a].releaseDate);
+			const dateB = new Date(albums[b].releaseDate);
 			if (dateA.getTime() === dateB.getTime()) {
 				// If release dates are the same, sort by catalog number in descending order
-				return albumsJson[b].id.localeCompare(albumsJson[a].id);
+				return albums[b].id.localeCompare(albums[a].id);
 			} else {
 				return dateB.getTime() - dateA.getTime();
 			}
 		})
-		.forEach((key) => {
-			albums[key] = albumsJson[key];
-		});
+		.reduce((obj, key) => {
+			obj[key] = albums[key];
+			return obj;
+		}, {});
 
-	return (
-		<>
-			<Layout>
-				<Head>
-					<title>{WEBSITE_NAME}</title>
-				</Head>
-				<Container>
-					<Header />
-					<div className="container pt-10 px-6 mx-auto">
-						<div className="flex flex-wrap">
-							{Object.keys(albums).map((key) => (
-								<AlbumListing key={key} slug={key} />
-							))}
-						</div>
-					</div>
-				</Container>
-			</Layout>
-		</>
-	);
+  const sortedLegacyAlbums = Object.keys(legacyAlbums)
+		.sort((a, b) => {
+			const dateA = new Date(legacyAlbums[a].releaseDate);
+			const dateB = new Date(legacyAlbums[b].releaseDate);
+			if (dateA.getTime() === dateB.getTime()) {
+				// If release dates are the same, sort by catalog number in descending order
+				return legacyAlbums[b].id.localeCompare(legacyAlbums[a].id);
+			} else {
+				return dateB.getTime() - dateA.getTime();
+			}
+		})
+		.reduce((obj, key) => {
+			obj[key] = legacyAlbums[key];
+			return obj;
+		}, {});
+
+  return (
+    <Layout>
+      <Head>
+        <title>{WEBSITE_NAME}</title>
+      </Head>
+      <Container>
+        <Header />
+        <div className="container pt-10 px-6 mx-auto">
+          <div className="flex flex-wrap">
+            {Object.keys(sortedAlbums).map((key) => (
+              <AlbumListing key={key} slug={key} />
+            ))}
+          </div>
+          <h2 className="text-lg font-bold mb-4">LEGACY ALBUMS</h2>
+          <div className="flex flex-wrap">
+            {Object.keys(sortedLegacyAlbums).map((key) => (
+              <AlbumListing key={key} slug={key} />
+            ))}
+          </div>
+        </div>
+      </Container>
+    </Layout>
+  );
 }
